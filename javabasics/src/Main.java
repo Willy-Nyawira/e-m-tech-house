fd3fd8f52efaaadb2cc5d8370d4f89aa6ade9526
Willy.nyawira <nyawiramwangi394@gmail.com>
2024-01-25 10:17:33 +0300

    first dev commit

--- javabasics/src/Main.java
+++ javabasics/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int age = 25;
-        System.out.println(age);
+        String name = "Robert";
+        System.out.println(name);
     }
 }
